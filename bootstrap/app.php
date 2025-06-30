199e095f2ca270e85e8c1f95b42b6d53d60673f2
Beslan066 <64842276+Beslan066@users.noreply.github.com>
2025-06-30 14:59:02 +0300

    Добавил middleware для админ части

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\UpdateLastSeenMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,8 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\App\Http\Middleware\UpdateLastSeenMiddleware::class);
+        // Глобальные middleware (выполняются на каждом запросе)
+        $middleware->append(UpdateLastSeenMiddleware::class);
 
+        // Регистрируем middleware для использования в маршрутах (аналог $routeMiddleware)
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Home\UserController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SentenceController;
+use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,38 +19,34 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
-    Route::post('/upload', [SentenceController::class, 'upload'])->name('sentences.upload');
-    Route::get('/logs/view', [SentenceController::class, 'getLogs'])->name('logs.view');
+    Route::post('/upload', [SentenceController::class, 'upload'])->name('sentences.upload')->middleware(Admin::class);
+    Route::get('/logs/view', [SentenceController::class, 'getLogs'])->name('logs.view')->middleware(Admin::class);
 
     Route::post('/sentences/{sentence}/approve', [SentenceController::class, 'approveTranslation'])->name('sentences.approve');
     Route::post('/sentences/{sentence}/delay', [SentenceController::class, 'delayTranslation'])->name('sentences.delay');
     Route::post('/sentences/{sentence}/reject', [SentenceController::class, 'rejectTranslation'])->name('sentences.reject');
-    Route::get('/sentences/moderating', [SentenceController::class, 'moderate'])->name('sentence.moderate');
+    Route::get('/sentences/moderating', [SentenceController::class, 'moderate'])->name('sentence.moderate')->middleware(Admin::class);
     Route::get('/sentences/completed', [HomeController::class, 'completedSentences'])->name('sentence.completed');
     Route::get('/sentences/district', [HomeController::class, 'districtSentences'])->name('sentence.district');
     Route::patch('/sentences/{sentence}', [SentenceController::class, 'resetTeacherSentence'])->name('sentence.update');
     Route::delete('/sentences', [HomeController::class, 'deleteSentences'])->name('sentences.delete');
     Route::get('/search', [HomeController::class, 'search'])->name('sentences.search');
 
-    Route::post('/export/sentences', [SentenceController::class, 'exportSentences']);
+    Route::post('/export/sentences', [SentenceController::class, 'exportSentences'])->middleware(Admin::class);
     Route::get('/export/progress/{batchId}', [SentenceController::class, 'checkExportProgress']);
     Route::get('/export/download/{filename}', [SentenceController::class, 'downloadExport']);
 
 
-
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::patch('/users/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::patch('/users/{user}/role', [UserController::class, 'updateRole'])->name('users.updateRole');
-    Route::delete('/users/{user}', [UserController::class, 'deleteUser'])->name('user.delete');
-    Route::get('/users/export', [UserController::class, 'export'])->name('users.export');
-    Route::get('/users/search', [UserController::class, 'search'])->name('users.search');
-    Route::get('/users/sentences/{user}', [UserController::class, 'userPage'])->name('users.page');
-
-
-
-
-
+    Route::group(['middleware' => ['auth', Admin::class]], function () {
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::patch('/users/{user}', [UserController::class, 'update'])->name('users.update');
+        Route::patch('/users/{user}/role', [UserController::class, 'updateRole'])->name('users.updateRole');
+        Route::delete('/users/{user}', [UserController::class, 'deleteUser'])->name('user.delete');
+        Route::get('/users/export', [UserController::class, 'export'])->name('users.export');
+        Route::get('/users/search', [UserController::class, 'search'])->name('users.search');
+        Route::get('/users/sentences/{user}', [UserController::class, 'userPage'])->name('users.page');
+    });
 
     Route::get('/translate', [SentenceController::class, 'getSentence'])->name('translate');
 
